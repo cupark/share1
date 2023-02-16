ae2b16a6e6d417facb998dfb0103b211f79f8f53
Park Chan-Uk <114058953+cupark@users.noreply.github.com>
2023-02-16 17:40:00 +0900

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -1,4 +1,57 @@
 import pyaudio
+import numpy as np
+import librosa
+
+# 설정값
+CHUNK_SIZE = 1024       # 오디오 스트림에서 한 번에 읽어들일 샘플 수
+SAMPLE_RATE = 44100     # 샘플링 주파수
+N_MFCC = 20             # 추출할 MFCC 계수의 수
+HOP_LENGTH = 512        # MFCC 계산 시 사용할 윈도우 크기
+
+# 버퍼 초기화
+buffer = []
+buffer_samples = 0
+
+# PyAudio 오디오 스트림 초기화
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=SAMPLE_RATE, input=True, frames_per_buffer=CHUNK_SIZE)
+
+while True:
+    # 스트림에서 데이터 읽어들이기
+    data = stream.read(CHUNK_SIZE)
+    samples = np.frombuffer(data, dtype=np.int16)
+    
+    # 버퍼에 데이터 추가
+    buffer.append(samples)
+    buffer_samples += len(samples)
+    
+    # 2초간 데이터를 쌓으면 MFCC 계산
+    if buffer_samples >= SAMPLE_RATE * 2:
+        # 버퍼에서 모든 오디오 데이터 추출
+        audio = np.concatenate(buffer, axis=0)
+        # MFCC 계산
+        mfccs = librosa.feature.mfcc(y=audio, sr=SAMPLE_RATE, n_mfcc=N_MFCC, hop_length=HOP_LENGTH)
+        # 계산 결과 출력
+        print("MFCC shape: ", mfccs.shape)
+        # 버퍼 초기화
+        buffer = []
+        buffer_samples = 0
+
+# PyAudio 스트림 닫기
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+
+
+
+
+
+
+
+
+
+import pyaudio
 
 CHUNK_SIZE = 2048
 FORMAT = pyaudio.paInt16
