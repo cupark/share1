e009cf8cd894820a7cdc7520d91e0009736cb5d6
Park Chan-Uk <114058953+cupark@users.noreply.github.com>
2023-02-16 17:48:24 +0900

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -1,76 +1,68 @@
-import pyaudio
 import queue
 import threading
-import time
+import pyaudio
 import numpy as np
 import librosa
+from sklearn.svm import SVC
 
-# 파라미터 설정
-CHUNK = 1024
-FORMAT = pyaudio.paFloat32
-CHANNELS = 1
-RATE = 44100
-HOP_LENGTH = 512
-N_MFCC = 13
-N_FRAMES = 43
-BUFFER_LENGTH_SEC = 2
-BUFFER_LENGTH = int(RATE * BUFFER_LENGTH_SEC / HOP_LENGTH) * HOP_LENGTH
 
-# 버퍼 큐 초기화
-buffer_queue = queue.Queue(maxsize=int(BUFFER_LENGTH/CHUNK))
+# Parameters for audio processing
+RATE = 44100  # Sampling rate
+CHANNELS = 1  # Number of audio channels
+BLOCKSIZE = 1024  # Number of frames per audio block
+N_MFCC = 13  # Number of MFCC coefficients to extract
+HOP_LENGTH = BLOCKSIZE // 2  # Hop length for the spectrogram
 
-# 스트림 콜백 함수
-def stream_callback(in_data, frame_count, time_info, status):
-    # 스트림 데이터를 큐에 추가
-    buffer_queue.put(in_data)
+# Parameters for classification
+MODEL_PATH = "svm_model.pkl"
+CLASSES = ["class1", "class2", "class3"]  # Labels for classification
+
+# Create a queue to hold audio data
+data_queue = queue.Queue()
+
+# Load the SVM model
+svm_model = SVC(kernel='rbf', gamma='scale')
+svm_model = joblib.load(MODEL_PATH)
+
+# Function to process audio data from the queue
+def process_data():
+    audio_data = np.array([])
+
+    # Read audio data from the queue until the buffer is full
+    while len(audio_data) < 2 * RATE:
+        try:
+            data = data_queue.get(block=False)
+            audio_data = np.concatenate((audio_data, data))
+        except queue.Empty:
+            pass
+
+    # Extract MFCC coefficients from the audio data
+    mfccs = librosa.feature.mfcc(y=audio_data, sr=RATE, n_mfcc=N_MFCC, hop_length=HOP_LENGTH)
+
+    # Classify the audio using the SVM model
+    mfccs = mfccs.reshape(1, -1)  # Reshape to fit the model input
+    predicted_class = svm_model.predict(mfccs)[0]
+
+    print("Predicted class:", CLASSES[predicted_class])
+
+# Function to read audio data from the microphone and put it in the queue
+def audio_callback(in_data, frame_count, time_info, status):
+    audio_data = np.frombuffer(in_data, dtype=np.float32)
+    data_queue.put(audio_data)
     return (in_data, pyaudio.paContinue)
 
-# MFCC 계산 함수
-def calculate_mfcc(buffer):
-    signal = np.frombuffer(buffer, dtype=np.float32)
-    mfccs = librosa.feature.mfcc(signal, sr=RATE, hop_length=HOP_LENGTH, n_mfcc=N_MFCC, n_fft=HOP_LENGTH*N_FRAMES)
-    mfccs_delta = librosa.feature.delta(mfccs)
-    mfccs_delta2 = librosa.feature.delta(mfccs, order=2)
-    return np.concatenate((mfccs, mfccs_delta, mfccs_delta2))
+# Create an audio stream from the microphone
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paFloat32,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=BLOCKSIZE,
+                    stream_callback=audio_callback)
 
-# MFCC 처리 함수
-def process_mfcc(buffer_queue):
-    while True:
-        # 버퍼 큐에서 2초간의 데이터 가져오기
-        buffer = b""
-        while buffer_queue.qsize() < int(BUFFER_LENGTH/CHUNK):
-            time.sleep(0.1)
-        for i in range(int(BUFFER_LENGTH/CHUNK)):
-            buffer += buffer_queue.get()
-        # MFCC 계산
-        mfccs = calculate_mfcc(buffer)
-        # 결과 출력
-        print(mfccs)
-
-# PyAudio 초기화
-p = pyaudio.PyAudio()
-
-# 입력 스트림 열기
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK,
-                stream_callback=stream_callback)
-
-# MFCC 처리 스레드 시작
-mfcc_thread = threading.Thread(target=process_mfcc, args=(buffer_queue,))
-mfcc_thread.daemon = True
-mfcc_thread.start()
-
-# 입력 스트림 시작
+# Start the audio stream
 stream.start_stream()
 
-# 입력 스트림 종료 대기
-while stream.is_active():
-    time.sleep(0.1)
-
-# PyAudio 종료
-stream.stop_stream()
-stream.close()
-p.terminate()
+# Process audio data in a separate thread
+while True:
+    threading.Thread(target=process_data).start()
